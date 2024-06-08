debcc4bf76d3f4017eb80ccebf8faba318b27968
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-06-08 16:49:39 +0900

    2024/6/8

---
+++ c언어.cpp
@@ -0,0 +1,57 @@
+//#include <stdio.h>
+//
+//int main(  )
+//{
+//	printf("씨큐브코딩은");
+//	printf("내일을 위해 사과나무를 심는것이다.");
+//	return 0; 
+//}
+
+//#include <stdio.h>
+//
+//int main( )
+//{
+//	printf("상봉초등학교/n");
+//	printf("5학년 5반/n");
+//	printf("조영웅/n") ;
+//  return 0; 
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("Hello\nC3coding");
+//	printf("Hello\bC3coding");
+//	printf("Hello\rC3coding");
+//	printf("Hello\tC3coding");
+//	
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("%%Solar Tracking System%%\n");
+//	printf("\\Smart Devices\\\n");
+//	printf("\"Matrix\"\n");
+//	printf("\'Hellow Robot\'\n");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("\'발명왕\'토마스 에디슨은 다음과 같이 말했다.\n");
+//	printf("\"천재는1%%의 영감과 99%%의 노력으로 이루어진다.\"");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("\\    /\\\n )  ( \)\n(  /  )\n \\(__)ㅣ");
+//}
+
+#include <stdio.h>
+int main( )
+{
+	printf("┌──────┐");
+	printf("│		│\n") ;
+}	
