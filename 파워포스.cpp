890bb097d99e36c62ee10e58483f44124899bf1f
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-06-22 12:47:15 +0900

    2024/6/22

---
+++ 파워포스.cpp
@@ -0,0 +1,151 @@
+//#include <stdio.h>
+//int main(int argc,char*argvs[])
+//{
+//	printf("안녕! 씨큐브코딩");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("씨큐브코딩은 ");
+//	printf("내일을 위해 사과나무를 심는 것이다.");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("씨큐브코딩은 ");
+//	printf("보다 나은 세상을 만드는 도구이다.");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("홍길동");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("홍길동 ");
+//	printf("김철수 ");
+//	printf("이영희");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("상봉초등학교 5학년 ");
+//	printf("5반 ");
+//	printf("조영웅");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("컴퓨터 과학의 아버지 ");
+//	printf("\"앨런 튜링\"");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("\"Computer Operation\"");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("%%Solar Tracking Cystem%%\n");
+//	printf("\\Smart Devices\\\n");
+//	printf("\"Matrix\"\n");
+//	printf("\'Hello Robot\'");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("월드 와이드 웹의 창시자\n     팀 버너스 리");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("******************************\n");
+//	printf("S*****************************\n");
+//	printf("******************************\n");
+//	printf("******************************\n");
+//	printf("******************************\n");
+//	printf("******************************\n");
+//	printf("******************************\n");
+//	printf("******************************\n");
+//	printf("*****************************E\n");
+//	printf("******************************");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{ 
+//	printf("12345678\n");
+//	printf("ABCDEFG\n");
+//	printf("대한민국\n");
+//	printf("abcdefg\n");
+//	printf("@#$^&*!\n");
+//	printf("12345678\n");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("ABCDEFG\n1234567");
+//	printf("\n\n");
+//	printf("abcdefg\r1234");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("1.\t2.\t3\t\n");
+//	printf("CPU\tMemory\tHDD");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("ABCDEFG123@#$");
+//	printf("\n\n");
+//	printf("ABC\babc\b123\b@#$");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("##\n");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("\'발명왕\' 토마스 에디슨은 다음과 같이 말했다.\n");
+//	printf("\"천재는 1%%의 영감과 99%%의 노력으로 이루어진다.\"");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("%d",12);
+//	printf("12");
+//	printf("%f",3.141592);
+//	printf("3.142592");
+//}
+
+//#include <stdio.h>
+//int main( )
+//{
+//	printf("내 나이는 : %d 입니다.",12);
+//	printf("나의 키는 : %f cm입니다.\",145.89);
+//	printf("나의 혈액형은 : %c 형입니다.", 'A');
+//}
