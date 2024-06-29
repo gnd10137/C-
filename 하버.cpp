b1bed18055be04bea1465c8543e6e58274f26212
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-06-29 16:50:24 +0900

    2024/6/29

---
+++ 하버.cpp
@@ -0,0 +1,74 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b;
+//	scanf("%d %d",&a,&b);
+//	
+//	printf("%d + %d = %d\n",a,b,a+b);
+//	printf("%d - %d = %d\n",a,b,a-b);
+//	printf("%d x %d = %d\n",a,b,a*b);
+//	printf("%d / %d = %d\n",a,b,a/b);
+//	printf("%d %% %d = %d\n",a,b,a%b);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b;
+//	scanf("%f %f",&a,&b);
+//	
+//	printf("%f + %f = %f\n",a,b,a+b);
+//	printf("%f - %f = %f\n",a,b,a-b);
+//	printf("%f x %f = %f\n",a,b,a*b);
+//	printf("%f / %f = %f\n",a,b,a/b);
+//	printf("%f %% %f = %f\n",a,b,a%b);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%d\n", 10/3);
+//	printf("%d\n", 10/3.0);
+//	printf("%f\n", 10/3.0);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a, b, c;
+//	
+//	printf("정수 입력:");
+//	scanf("%d", &a);
+//	printf("정수 입력:");
+//	scanf("%d", &b);
+//	printf("%d\n", 10%3);
+//	printf("%d\n", a%10);
+//	c = a%b;
+//	printf("%d\n",c);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a;
+//	scanf("%d",&a);
+//	printf("%d", a%10 + a/10);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a;
+//	scanf("%d",&a);
+//	printf("%d\n",a%10 + a/100 + (a/10)%10);
+//	printf("%d",(a%10) * (a/100) * ((a/10)%10));
+//}
+
+#include <stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	printf("%d",a&7);
+}
+5
