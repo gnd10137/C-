11c3240c3e63ca179f38352d4335447979b8a0ac
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-07-06 16:51:05 +0900

    2024/7/6

---
+++ 페이드.cpp
@@ -0,0 +1,97 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b;
+//	scanf("%d %d",&a,&b);
+//	printf("%d",a+b);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("\"천재는 1%%의 영감과 99%%의 노력으로 이루어진다\"\n	-토머스 에디슨");
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b;
+//	scanf("%d %d",&a,&b);
+//	
+//	printf("%d + %d = %d\n",a,b,a+b);
+//	printf("%d - %d = %d\n",a,b,a-b);
+//	printf("%d x %d = %d\n",a,b,a*b);
+//	printf("%d / %d = %d\n",a,b,a/b);
+//	printf("%d %% %d = %d\n",a,b,a%b);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a;
+//	scanf("%d",&a);
+//	printf("%d\n",a%10 + a/100 + (a/10)%10);
+//	printf("%d",(a%10) * (a/100) * ((a/10)%10));
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b
+//	printf("정수 1:");
+//	scanf("%d"&a);
+//	printf("\정수 2:");
+//	scanf("%d",&b);
+//	
+//	printf("\n");
+//	printf("%d + %d = %d\n",a,b,a+b);
+//	printf("%d - %d = %d\n",a,b,a-b);
+//	printf("%d x %d = %d\n",a,b,a*b);
+//	printf("%d / %d = %d\n",a,b,a/b);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b,c,d;
+//	scanf("%d",&a);
+//
+//	b = a/100;
+//	c = a%100/ 10;
+//	d = a%10
+//
+//	printf("%d+%d+%d=%d",b,c,d,b+c+d);
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%d\n",(10>5) && (8<3));
+//	printf("%d\n",(10>5) || (8<3));
+//	printf("%d\n", !(10>5));
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int res;
+//	
+//	res= 0 && 0;
+//	printf("%d\n",res);
+//	res= 0 && 1;
+//	printf("%d\n",res);
+//	res= 1 && 0;
+//	printf("%d\n",res);
+//	res= 1 && 1;
+//	printf("%d\n",res);
+//	
+//	res= 0 || 0;
+//	printf("%d\n",res);
+//	res= 0 || 1;
+//	printf("%d\n",res);
+//	res= 1 || 0;
+//	printf("%d\n",res);
+//	res= 1 || 1;
+//	printf("%d\n",res);
+//	
+//	printf("%d\n", !res);
+//}
