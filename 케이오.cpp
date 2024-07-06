f80e5f89882bfa457d7451d124dfe95aa7369c99
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-07-06 12:50:12 +0900

    2024/7/6

---
+++ 케이오.cpp
@@ -0,0 +1,43 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b;
+//	a=23;
+//	b=24;
+//	printf("%d\n",a>b);
+//	printf("%d\n",a<b);
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%d\n", 10>5) && (8<3);
+//	printf("%d\n", 10>5) || (8<3);
+//	printf("%d\n", ! (10>8));
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int res;
+//	
+//	res=0 && 0;
+//	printf("%d\n",res);
+//	res=0 && 1;
+//	printf("%d\n",res);
+//	res=1 && 0;
+//	printf("%d\n",res);
+//	res=1 && 1;
+//	printf("%d\n",res);
+//	
+//	res=0 || 0;
+//	printf("%d\n",res);
+//	res=0 || 1;
+//	printf("%d\n",res);
+//	res=1 || 0;
+//	printf("%d\n",res);
+//	res=1 || 1;
+//	printf("%d\n",res);
+//	
+//	printf("%d\n", !res);
+//}
