09292a2c753ab5d11355a13e9dea956bbd684d12
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-07-20 16:49:02 +0900

    2024/7/20

---
+++ 레이나.cpp
@@ -0,0 +1,169 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b;
+//	scanf("%d %d",&a,&b);
+//	if(a>b)
+//	{
+//		printf("a가 b보다 크다.");
+//	}
+//	else
+//	{
+//		printf("a가 b보다 작다.");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b;
+//	
+//	a=12;
+//	b=56;
+//	
+//	if(a>b)
+//	{
+//		printf("%d는 %d보다 크다\n",a,b);
+//	}
+//	else
+//	{
+//		printf("%d는 %d보다 크지 않다\n",a,b);
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int num;
+//	
+//	printf("10000 미만 정수 입력:");
+//	scanf("%d",&num);
+//	
+//	if(num<10)
+//	{
+//		printf("한 자리 수\n");
+//	}
+//	else if(num<100)
+//	{
+//		printf("두 자리 수\n");
+//	}
+//	else if(num<1000)
+//	{
+//		printf("세 자리 수\n");
+//	}
+//	else if(num<10000)
+//	{
+//		printf("네 자리 수\n");
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a;
+//	scanf("%d",&a);
+//	if(a>0)
+//	{
+//		printf("양수\n");
+//	}
+//	else
+//	{
+//		printf("음수\n");
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a;
+//	scanf("%d",&a);
+//	if(a%2==0)
+//	{
+//		printf("짝수\n");
+//	}
+//	else
+//	{
+//		printf("홀수\n");
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a;
+//	scanf("%d",&a);
+//	if(a%3==0)
+//	{
+//		printf("3의 배수입니다.\n");
+//	}
+//	else
+//	{
+//		printf("3의 배수가 아닙니다.\n");
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b;
+//	scanf("%d %d",&a,&b);
+//	if(a>b && a%b==0)
+//	{
+//		printf("나누어 떨어집니다.\n");
+//	}
+//	else if(b>a && b%a==0) 
+//	{
+//		printf("나누어 떨어집니다.");
+//	}
+//	else
+//	{
+//		printf("나누어 떨어지지 않습니다.");
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b;
+//	scanf("%d %d",&a,&b);
+//	if(a%2==0 && b%2==0)
+//	{
+//		printf("%d",a*b);
+//	}
+//	else if(a%2==1 && b%2==1)
+//	{
+//		printf("%d",a+b);
+//	}
+//	else
+//	{
+//		printf("%d",a-b);
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a;
+//	scanf("%d",&a);
+//	if(a>=90 && a<100)
+//	{
+//		printf("A");
+//	}
+//	else if(a>=80 && a<90)
+//	{
+//		printf("B");
+//	}
+//	else if(a>70 && a<80)
+//	{
+//		printf("C");
+//	}
+//	else if(a>60 && a<70)
+//	{
+//		printf("D");
+//	}
+//	else
+//	{
+//		printf("F");
+//	}
+//}
