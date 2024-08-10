b61917f3e44aa3e6dbd6aeee507f174f64ac24c3
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-08-10 12:49:29 +0900

    2024/8/10

---
+++ 브림스톤.cpp
@@ -0,0 +1,146 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	
+//	n=-8;
+//	while (n<=1)
+//	{
+//		printf("%d",n);
+//		n++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	
+//	n=12;
+//	while (n<=20)
+//	{
+//		printf("%d",n);
+//		n++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	
+//	n=10;
+//	while (n<=17)
+//	{
+//		printf("%d",n);
+//		n++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	
+//	n=12;
+//	while (n<=20)
+//	{
+//		printf("%d",n);
+//		n++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int i;
+//	
+//	i=1;
+//	while (i<=10)
+//	{
+//		printf("%d",i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int i;
+//	i=1;
+//	do
+//	{
+//		printf("%d",i);
+//		i++;
+//	} while (i<=10);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int i;
+//	
+//	for(i=1; i<=10; i++)
+//	{
+//		printf("¾¾Å¥ºêÄÚµù");
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	
+//	for (n=-10;n<=-1; n++)
+//	{
+//		printf("%d",n);
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	
+//	for (n=30; n>20; n--)
+//	{
+//		printf("%d\n",n);
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	
+//	for (n=1; n<=100; n++)
+//	{
+//		printf("%d\n",n);
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	for (n=-1; n=n; n++)
+//	{
+//		printf("%d\n",n);
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	for (n=1; n<=n; n++)
+//	{
+//		printf("%d\n",n);
+//	}
+//}
