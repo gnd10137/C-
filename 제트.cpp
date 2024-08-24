804f43112b968dbf16243f8e9842c5caed209394
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-08-24 12:48:00 +0900

    2024/8/24

---
+++ 제트.cpp
@@ -0,0 +1,215 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int i;
+//	
+//	i=1;
+//	for ( ; i<=10; )
+//	{
+//		printf("%d",i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int i;
+//	
+//	i=1;
+//	while (i <= 10)
+//	{
+//		printf("%d", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	
+//	n=-8;
+//	while (n<=1)
+//	{
+//		printf("%d",n);
+//		n++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	
+//	n=12;
+//	while (n<=20)
+//	{
+//		printf("%d\n",n);
+//		n++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	
+//	n=10;
+//	while (n<=17)
+//	{
+//		printf("%d\n",n);
+//		n++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	
+//	n=20;
+//	while (n>=15)
+//	{
+//		printf("%d\n",n);
+//		n--;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int i;
+//	i = 1;
+//	while (i <= 10)
+//	{
+//		printf("%d\n", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int i;
+//	
+//	i = 1;
+//	do
+//	{
+//		printf("%d\n", i);
+//		i++;
+//	}while (i <= 10);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n;
+//	
+//	n=2;
+//	switch (3)
+//	{
+//	case 0:
+//		printf("영입니다.");
+//	case 1:
+//		printf("일입니다.");
+//	case 2:
+//		printf("이입니다.");
+//	case 3:
+//		printf("삼입니다.");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//	int i;
+//	int o,e;
+//	int n,m;
+//	
+//	for (i = 1; i <= 10; i++)
+//	{
+//		n = i;
+//		printf("%d",n);
+//		
+//		if (i % 2 == 1)
+//		{
+//			o = i;
+//			printf("%d ",o);
+//		}
+//		else
+//		{
+//			e = i;
+//			printf("%d ", e);
+//		}
+//		m = i;
+//		printf("%d ", m);
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//	int i;
+//	int o,p,q;
+//	int n,m;
+//	
+//	for (i=1; i<=10; i++)
+//	{
+//		n=i;
+//		printf("%d",n);
+//		
+//		if (i%2==0)
+//		{
+//			o=i;
+//			printf("%d ",o);
+//		}
+//		if (i%3==0)
+//		{
+//			p=i;
+//			printf("%d ",p);
+//		}
+//		if (i%4==0)
+//		{
+//			q=i;
+//			printf("%d ",q);
+//		}
+//		m=i;
+//		printf("%d ",m);
+//	}
+//	return 0;
+//}
+
+#include <stdio.h>
+int main()
+{
+	int n;
+	
+	n=2;
+	switch (3)
+	{
+	case 0:
+		printf("영입니다.");
+	case 1:
+		printf("일입니다.");
+	case 2:
+		printf("이입니다.");
+	case 3:
+		printf("삼입니다."); 
+	}
+	return 0;
+}
