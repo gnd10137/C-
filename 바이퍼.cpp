7433c013380cfea6c7c9d5eee5d660d54c724113
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-08-31 16:55:11 +0900

    2024/8/31

--- 바이퍼.cpp
+++ 바이퍼.cpp
@@ -1,120 +1,114 @@
 //#include <stdio.h>
 //int main()
 //{
-//	int n;
-//	int sum=0;
-//	int count=0;
-//	scanf("%d",&n);
-//	for (int i=1;i<=n;i++){
-//		if(i%6==0){
-//			count++;
-//			sum += i;
-//		}
-//	}
-//	printf("갯수 : %d",count);
-//	printf("합 : %d",sum);
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//	int i=10;
-//	int count=0;
-//	int sum=0;
-//	while(i<=17)
+//	for(int i = 1; i < 7; i++)
 //	{
-//		count++;
-//		
-//		sum+=i;
-//		printf("%d ",i);
-//		i++;
-//	}
-//	printf("반복횟수:%d\n",count);
-//	printf("합:%d",sum);
-//	return 0;
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//	int i=11;
-//	do
-//	{
-//		printf("%d ",i);
-//		i++;
-//	}
-//	while (i<=10);
-//	return 0;
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//	int i=0;
-//	while(i<10)
-//	{
-//		printf("i의 값:%d\n",i);
-//		i++;
-//		if(i==5)
+//		printf("%d 학년", i);
+//		for (int j = 7; j >= 1; j--)
 //		{
-//			break;
+//			printf("%d 반",j);
+//		}
+//		printf("\n");
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	for(int i = 1; i <= 5; i++)
+//	{
+//	
+//		for (int j = 1; j <= i; j++)
+//		{
+//			printf("%d", j);
+//		}
+//		printf("\n");
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	for(int i = 5; i >=1; i--)
+//	{
+//		for (int j = 1; j <= i; j++)
+//		{
+//			printf("%d",j);
+//		}
+//		printf("\n");
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	for(int i = 5; i >= 1; i--)
+//	{
+//		for (int j = i; j <= 5; j++)
+//		{
+//			printf("%d", j);
+//		}
+//		printf("\n");
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	for(int i = 5; i >= 1; i--)
+//	{
+//		for (int j = i; j >= 1; j--)
+//		{
+//			printf("%d", j);
+//		}
+//		printf("\n");
+//	}
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int cnt=0;
+//	int max=0;
+//	int final;
+//	for (int i = 1; i <= 1000; i++)
+//		
+//	{
+//		cnt=0;
+//		printf("%d :", i);
+//		for(int j = 1; j <= i; j++)
+//		{
+//			if(i % j == 0)
+//			{
+//				printf("%d ",j);
+//				cnt++;
+//			}
+//		}
+//		if(max<cnt){
+//			max=cnt;
+//			final=i;
+//		}
+//		printf("\n");
+//	}
+//	printf("\n final : %d\n", final);
+//	for(int i = 1; i <= final; i++){
+//		if(final % i == 0){
+//			printf("820의 약수 : %d ",i);
 //		}
 //	}
+//	printf(" 약수의 개수 : %d",max);
 //}
 
 //#include <stdio.h>
 //int main()
 //{
-//	int n;
-//	scanf("%d",&n);
-//	while (n>0){
-//		count++;
-//		n\=10;
+//	for (int i=5; i>=1; i++)
+//	{
+//		for (int j=4; j>=i; j--)
+//		{
+//			printf("  ");
+//		}
+//		for (int j=i; j>=1; j--)
+//		printf("%d ", j)
 //	}
-//	printf("%d자리수",count);
 //}
-
-//#include <stdio.h>
-//int main()
-//{
-//	int n;
-//	int sum=0;
-//	scanf("%d",&n);
-//	while (n>0){
-//		printf("%d",n%10);
-//		sum+=n%10;
-//		n/=10;
-//	}
-//	printf("\n합:%d",sum);
-//	
-//}
-
-//#include <stdio.h>
-//int main()
-//{
-//	int n, u, reverse = 0;
-//	scanf ("%d",&n);
-//	u = n;
-//	while (n > 0){
-//		reverse = n * 10 + n % 10;
-//		n /= 10;
-//	}
-//	printf("%d을 뒤집은 정수는 : %d\n",u,reverse);
-//	printf("두 수의 합은 : %d",);
-//}
-
-#include <stdio.h>
-int main()
-{
-	int a,b;
-	int cnt=0,sum=0;
-	scanf("%d %d",&a,&b);
-	
-	for (int i = a - 1; i <= b + 1; i--){
-		printf("%d",i);
-		cnt++;
-		sum += i;
-	}
-	printf("%d와 %d사이 수의 개수는 : %d",a,b,cnt);
-	printf("%d와 %d사이 수의 합은 : %d",a,b,sum);
-}
