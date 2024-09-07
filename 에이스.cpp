3f22f0b39d2e4c2c8d69e5b88efb9e7185ad19ca
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-09-07 12:52:12 +0900

    2024/9/7

---
+++ 에이스.cpp
@@ -0,0 +1,59 @@
+//#include <stdio.h>
+//int main()
+//{
+//	double height[4];
+//	int j=2;
+//	
+//	height[0]=145.7;
+//	height[1]=156.3;
+//	height[j]=147.89;
+//	j++;
+//	height[j]=159.4;
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char c; int i; long l; float f; double d;
+//	
+//	printf("%d %d %d %d %d\n",sizeof(c), sizeof(i), sizeof(l), sizeof(f), sizeof(d));
+//	printf("%d %d %d\n", sizeof(char), sizeof(int), sizeof(long));
+//	printf("%d %d\n", sizeof(float), sizeof(double));
+//	printf("%d %d\n",sizeof(153), sizeof(5.2));
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char ch;
+//	
+//	printf("문자를 입력하세요 : ");
+//	scanf("%c", &ch);
+//	printf("%c %d\n", ch, ch);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char ch;
+//	
+//	printf("문자를 입력하세요. : ");
+//	scanf("%c",&ch);
+//	printf("%c %d\n", ch, ch);
+//	return 0;
+//}
+
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	
+	for (int n; 1<=n n++)
+	{
+		printf("%d ",n);
+	}
+}
