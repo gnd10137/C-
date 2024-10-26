94e79be5d7d9f46a1f14e1dab065fed95f2c7cb0
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2024-10-26 11:46:02 +0900

    2024/10/26

---
+++ 세이지.cpp
@@ -0,0 +1,67 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int ar[10] = {10, 20, 30, 40, 50, 60, 70, 80,90, 100};
+//	int br[7];
+//	
+//	printf("배열의 이름 : %d\n, ar");
+//	printf("배열의 이름 : %d\n, br");
+//	return 0;
+//}
+
+//#include <stdio.h> 
+//int main()
+//{
+//	int ar[3] = {10, 20, 30};
+//	printf("%d\n", ar[0]);
+//	printf("%d\n", ar+0);
+//	printf("%d\n", &ar[0]);
+//	printf("%d\n", *(ar+0));
+//	printf("%d\n", *ar);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int n, *p;
+//	p = &n;
+//	*p = 70;
+//	printf("%d %d", n, *p);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int ar = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+//	int i;
+//	
+//	*(ar+0) = 34;
+//	*(ar+1) = 21;
+//	
+//	printf("%d %d\n", ar[0], *(ar+0));
+//	printf("%d %d\n", ar[1], *(ar+1));
+//	printf("%d %d", *(ar+2), *(ar+3));
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int ar[3] = {10, 20, 30};
+//	int i;
+//	
+//	*(ar+0) = 34;
+//	*(ar+1) = 21;
+//	printf("%d %d \n", ar[0], *(ar+0));
+//	printf("%d %d \n", ar[1], *(ar+1));
+//	printf("%d %d \n", *(ar+2), *(ar+3));
+//}
+
+#include <stdio.h>
+int main()
+{
+	
+}
