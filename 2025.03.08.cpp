48b2814524e754c27317a1e613b9dee1a4d0e322
gnd10137 <154660760+gnd10137@users.noreply.github.com>
2025-03-08 16:40:01 +0900

    2025-03-08

---
+++ 2025.03.08.cpp
@@ -0,0 +1,40 @@
+//#include <cstdio>
+//int main()
+//{
+//	char C; int I; long long L; double D; char S[101];
+//	scanf("%c %d %lld %lf %s",&C,&I,&L,&D,S);
+//	printf("%s %lf %lld %d %c",S,D,L,I,C);
+//	return 0;
+//}
+
+//#include <cstdio>
+//int main()
+//{
+//	int H,M,S,T;
+//	scanf("%d %d %d",&H,&M,&S);
+//	scanf("%d", &T);
+//	S-=T%60;
+//	if (S<0) {
+//		S+=60;
+//		M--;
+//	}
+//	
+//	M-=T%60;
+//	T/=60;
+//	H-=T;
+//	if (M<0) {
+//		M+=60;
+//		H--;
+//	}
+//	printf("%d %d %d",H,M,S);
+//	return 0;
+//}
+
+#include <cstdio>
+int main()
+{
+	long long n, m, i;
+	scanf("%lld %lld %lld",&n,&m,&i);
+	if ()
+	return 0;
+}
